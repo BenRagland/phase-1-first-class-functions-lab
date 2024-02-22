644c2b24eddfe961291d127685e1003067ba6df1
Benjamin <Officialbenragland@gmail.com>
2024-02-22 11:54:01 -0500

    Advanced Functions solutions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (driversArray)=>{
+    const firstTwoDrivers = [driversArray[0], driversArray[1]]
+    return firstTwoDrivers
+}
+
+const returnLastTwoDrivers = (driversArray) =>{
+    return driversArray.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// takes a fare and times by 5
+const createFareMultiplier = (fareMultiplier) =>{
+    return (fare) =>{
+        return fare * fareMultiplier
+    }
+    
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+  
+const selectDifferentDrivers = ( ScubersDriverList, twoDriversFunc ) => {
+    return twoDriversFunc (ScubersDriverList)
+
+}
+
+// const fareTripler = (x) => {
+//     const fare = createFareMultiplier(x)
+//     return fare(3)*3
+// }
